4c4c79fe4525142596d0f5bf7c9ac5b5d641d3fc
Jonathan Forney <jonathan.forney@gmail.com>
2020-05-07 22:38:59 -0700

    cool little querry string route thing

--- main.js
+++ main.js
@@ -9,12 +9,23 @@ app.get('/', (req, res) => {
   console.log('Req params, body, url & querry',
     req.params, req.body, req.url, req.querry);
   res.send('Hello Wierd World');
-})
-  .listen(port, () => {
+});
+
+app.get('/items/:veggie', (req, res) => {
+  let veggie = (req.params.veggie);
+  console.log('req.params', req.params, 'veggie', veggie);
+  res.send(`<h1>This is the page for the ${veggie}</h1>`);
+});
+
+app.post('/', (req, res) => {
+  res.send('Your contact information has been submitted succesfully');
+});
+
+app.listen(port, () => {
   /* eslint-disable-next-line no-console */
-    console.log(`--------------------------------------------------------------------
+  console.log(`--------------------------------------------------------------------
   Your Node and Express server has started on port ${port}. 
   Welcome to SkyNet
 --------------------------------------------------------------------`);
-  });
+});
 
