53b5a41f6753f4b109622b8a17c3583e72d6ebd0
Jonathan Forney <jonathan.forney@gmail.com>
2020-05-11 17:48:40 -0700

    adding dummy content to index.html

--- main.js
+++ main.js
@@ -1,8 +1,12 @@
+//  Imports and Server-level Variables
 const port = process.env.PORT || 3000,
   express = require('express'),
   app = express(),
-  logger = require('morgan');
+  logger = require('morgan'),
+  homeController = require('./controllers/homeController'),
+  jonnyLogger = require('./middleware/jonnyLogger');
 
+//  Instantiating Middleware
 app.use(logger('dev'));
 
 app.use(
@@ -13,23 +17,14 @@ app.use(
 
 app.use(express.json());
 
-app.use((req, res, next) => {
-  /* eslint-disable-next-line no-console */
-  console.log(`Loggin' Regest Info:
-  method: ${req.method}, url: ${req.url}`
-  // ${(req.params || req.params !== 'undefined') ? `params: ${req.params}` : null}
-  );
-  next();
-});
+app.use(jonnyLogger);
 
+// Routing
 app.get('/', (req, res) => {
   res.send('Hello Wierd World');
 });
 
-app.get('/items/:veggie', (req, res) => {
-  let veggie = (req.params.veggie);
-  res.send(`<h1>This is the page for the ${veggie}</h1>`);
-});
+app.get('/items/:veggie', homeController.veggieParser);
 
 app.post('/', (req, res) => {
   /* eslint-disable-next-line no-console */
