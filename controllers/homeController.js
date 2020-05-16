a8faf6c6398e5e3337aff18e5066b038ad7ff629
Jonathan Forney <jonathan.forney@gmail.com>
2020-05-15 17:52:23 -0700

    well it works now, no fucking clue why or what was important to change.... fuck fuck fuck fuckl fuck

--- controllers/homeController.js
+++ controllers/homeController.js
@@ -1,36 +1,12 @@
-const ejs = require('ejs'),
-  name = require('../views/name.ejs');
+let ejs = require('ejs'),
+  people = ['geddy', 'neil', 'alex'],
+  html = ejs.render('<%= people.join(", "); %>', { people: people }),
+  layout = require('../views/layout.ejs');
 
-
-exports.veggieParser = (req, res) => {
-  let veggie = (req.params.veggie);
-  res.send(`<h1>This is the page for the ${veggie}</h1>`);
-};
-
-exports.message = (req, res) => {
-  let rNumb = Math.floor(Math.random() * 8);
-  let messages = [
-    'today may be interesting, but wasnt is always?',
-    'group together, stronger as a feather, no matter the weather',
-    'i eat hawk bacon. and i wash it down with melted cheese',
-    'a good egg is a good thing to eat. but do not eat a rotten egg.',
-    'nobody really likes baloons... too dangerous!',
-    'eat meatloaf sandwich winner',
-    'agent cooper is on to you and your activities',
-    'good? bad? im the guy with the gun'
-  ];
-  let ourMessage = messages[rNumb];
-
-  res.send(`<h1>${ourMessage}</h1>`);
-};
+/* eslint-disable-next-line no-console */
+console.log('im in the home controller');
 
 exports.getName = (req, res) => {
   console.log('trying to get name...', res);
-  res.send(name);
-};
-
-exports.postContactInfo = (req, res) => {
-  /* eslint-disable-next-line no-console */
-  console.log('Req.body: ', req.body, 'Req.querry', req.query);
-  res.send('Your contact information has been submitted succesfully');
+  res.send(layout);
 };
--- main.js
+++ main.js
@@ -1,43 +1,13 @@
-//  Imports and Server-level Variables
 const 
   express = require('express'),
   app = express(),
-  port = process.env.PORT || 3000,
-  logger = require('morgan'),
-  homeController = require('./controllers/homeController'),
-  jonnyLogger = require('./middleware/jonnyLogger');
+  port = process.env.PORT || 3000;
 
 app.set('view engine', 'ejs');
-// app.set("view engine", "ejs");
-
-/* eslint-disable-next-line no-console */
-console.log('app.view engine:', app.get('view engine'));
-
-//  Instantiating Middleware
-app.use(logger('dev'));
-app.use(
-  express.urlencoded({
-    extended: false,
-  })
-);
-app.use(express.json());
-app.use(jonnyLogger);
-
-// Routing
-app.get('./name', homeController.getName);
 
 app.get('/', (req, res) => {
-  res.send('Hello Wierd World');
+  res.render('name');
 });
-
-app.get('/message.html', homeController.message);
-
-app.get('/items/:veggie', homeController.veggieParser);
-
-
-app.post('/', homeController.postContactInfo);
-
-app.post('/sign_up', homeController.postContactInfo);
 
 app.listen(port, () => {
   /* eslint-disable-next-line no-console */
@@ -46,4 +16,3 @@ app.listen(port, () => {
   Welcome to SkyNet
 --------------------------------------------------------------------`);
 });
-
