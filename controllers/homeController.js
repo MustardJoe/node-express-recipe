c22c43250d2392abdfed57373c9ec804ae1d7bc4
Jonathan Forney <jonathan.forney@gmail.com>
2020-05-20 17:32:41 -0700

    the instructions from the book just aren't working at all

--- controllers/homeController.js
+++ controllers/homeController.js
@@ -1,12 +1,17 @@
-let ejs = require('ejs'),
-  people = ['geddy', 'neil', 'alex'],
-  html = ejs.render('<%= people.join(", "); %>', { people: people }),
-  layout = require('../views/layout.ejs');
+const ejs = require('ejs');
+// const name = require('../views/name.ejs');
 
 /* eslint-disable-next-line no-console */
 console.log('im in the home controller');
 
-exports.getName = (req, res) => {
-  console.log('trying to get name...', res);
-  res.send(layout);
+// exports.getName = (req, res) => {
+//   console.log('trying to get name...', res);
+//   res.send(name.ejs);
+// };
+
+exports.respondWithName = (req, res) => {
+  let ourName = req.params.name;
+  console.log('ourName', ourName, 'req.params:', req.params);
+  res.render('name', { name: ourName });
 };
+
--- main.js
+++ main.js
@@ -1,15 +1,23 @@
 const 
   express = require('express'),
-  app = express(),
-  port = process.env.PORT || 3000;
+  app = express();
+const path = require('path');
+app.set('views', path.join(__dirname, 'views'));
+
 
 app.set('view engine', 'ejs');
+const port = process.env.PORT || 3000;
+const homeController = require('./controllers/homeController');  ;
+
+console.log('view engine', app.get('view engine'));
 
 app.use((req, res, next) => {
   /* eslint-disable-next-line no-console */
   console.log('req stuff', req.method, req.url);
   next();
 });
+
+app.get('/name/:name', homeController.respondWithName);
 
 app.get('/', (req, res) => {
   res.render('name');
